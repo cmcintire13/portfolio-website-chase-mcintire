a90184b3661543b604d41d766e471fc6c4b6999c
cmcintire13 <camcintire21@gmail.com>
2024-12-12 20:38:16 -0600

    all

--- semester-project/vue.config.js
+++ semester-project/vue.config.js
@@ -1,3 +1,6 @@
+// vue.config.js
 module.exports = {
-  publicPath: '/portfolio-website-chase-mcintire/', // Replace <repository-name> with your GitHub repo name
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/portfolio-website-chase-mcintire/' // Replace with your repository name
+    : '/'
 };
