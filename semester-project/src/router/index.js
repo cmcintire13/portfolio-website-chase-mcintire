145539ae4e62bef0a772db11212c153a09b994a4
cmcintire13 <camcintire21@gmail.com>
2024-12-12 19:56:20 -0600

    semester-project

---
+++ semester-project/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Homeview from '../views/Home-view.vue'
+import Portfolio from '../views/Portfolio.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Homeview
+  },
+  {
+    path: '/portfolio',
+    name: 'portfolio',
+    component: Portfolio
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
